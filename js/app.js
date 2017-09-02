589f48c9dac5ad8c31f38b4a4933ba7f95ddde2b
Vladimir <vdem@in-syst.ru>
2017-09-02 19:07:57 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-
+var $ = Dom7;
+var theme = 'auto';
+var app = new Framework7({
+  root: '#app',
+  theme: theme,
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+    };
+  },
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  routes: routes
+});
